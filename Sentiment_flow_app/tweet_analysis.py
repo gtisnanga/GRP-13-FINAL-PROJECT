ccadeb2993f8ad23de027fc1390b180f09d48d05
Angela W Maina <164763470+ANGI3MAINA@users.noreply.github.com>
2024-11-14 18:45:12 +0300

    app

--- Sentiment_flow_app/tweet_analysis.py
+++ Sentiment_flow_app/tweet_analysis.py
@@ -3,6 +3,7 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import pickle
+import gradio as gr
 
 # Download required NLTK data
 nltk.download('stopwords')
@@ -23,6 +24,7 @@ with open('./models/vectorizer.pkl', 'rb') as file:
 with open('./models/labels.pkl', 'rb') as file:
     label_categories = pickle.load(file)    
 
+# Define the text preprocessing function
 def preprocess_text(text):
     # Convert to lower case
     text = text.lower()
@@ -39,17 +41,37 @@ def preprocess_text(text):
     
     return ' '.join(cleaned_tokens)
 
+# Define the vectorization function
 def vectorize(clean_data):
     # Transform the cleaned text to TF-IDF representation using the loaded vectorizer
     text_tfidf = vectorizer.transform([clean_data])
     return text_tfidf
 
+# Define the prediction function
 def predict(tf_data):
     # Make prediction using the loaded model
     prediction = model.predict(tf_data)
     return label_categories[prediction[0]]
 
+# Define the main function that combines preprocessing, vectorization, and prediction
 def execute_flow(text):
     clean_texts = preprocess_text(text)
     vectorized_data = vectorize(clean_data=clean_texts)
-    return predict(vectorized_data)   
+    return predict(vectorized_data)
+
+# Define the Gradio interface function
+def sentiment_analysis(text):
+    # Run the execute_flow function to get the sentiment prediction
+    return execute_flow(text)
+
+# Set up the Gradio interface
+iface = gr.Interface(
+    fn=sentiment_analysis, 
+    inputs="text", 
+    outputs="text", 
+    title="Sentiment Analysis App",
+    description="Enter a sentence to determine its sentiment category."
+)
+
+# Launch the Gradio app
+iface.launch()    
